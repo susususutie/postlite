e18dcf3feb409ba04c312ba18bce77c1e7c3eb1a
sutie <susususutie@gmail.com>
2026-04-06 20:48:44 +0800

    fix: 将 CLI 服务器改为 ES Module 语法以兼容项目配置

--- bin/postlite.js
+++ bin/postlite.js
@@ -5,11 +5,15 @@
  * Usage: npx postlite [port]
  */
 
-const http = require('http');
-const https = require('https');
-const fs = require('fs');
-const path = require('path');
-const { exec } = require('child_process');
+import http from 'http';
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
+import { exec } from 'child_process';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // 禁止的主机和 IP 段
 const BLOCKED_HOSTS = [
